35dbd000a5df857e0926a2f65bae904b28404ce5
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-11-22 07:35:12 +0800

    Update HomeViewModel.kt and implementing game list function to HomeFragment.kt

--- app/src/main/java/com/edwinyosua/mobilegamestore/ui/home/HomeFragment.kt
+++ app/src/main/java/com/edwinyosua/mobilegamestore/ui/home/HomeFragment.kt
@@ -3,11 +3,17 @@ package com.edwinyosua.mobilegamestore.ui.home
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.edwinyosua.core.data.remote.network.ApiResponse
+import com.edwinyosua.core.ui.GameListAdapter
 import com.edwinyosua.mobilegamestore.base.BaseFragment
 import com.edwinyosua.mobilegamestore.databinding.FragmentHomeBinding
+import org.koin.android.ext.android.inject
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+
+    private val homeViewModel: HomeViewModel by inject()
+    private val gameListAdapter: GameListAdapter by inject()
 
     override fun getViewBinding(
         inflater: LayoutInflater,
@@ -16,10 +22,16 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
     ): FragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
 
     override fun initObserver() {
-        val homeViewModel = ViewModelProvider(this)[HomeViewModel::class.java]
 
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            binding.textHome.text = it
+        homeViewModel.gameList.observe(viewLifecycleOwner) { gameList ->
+            if (gameList != null) {
+                when (gameList) {
+                    ApiResponse.Empty -> {}
+                    is ApiResponse.Error -> {}
+                    ApiResponse.Loading -> {}
+                    is ApiResponse.Success -> gameListAdapter.submitList(gameList.data)
+                }
+            }
         }
     }
 
@@ -27,7 +39,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>() {
 
     override fun initAction() {}
 
-    override fun initUi() {}
+    override fun initUi() {
+        with(binding.rvGame) {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = gameListAdapter
+        }
+    }
 
     override fun initIntent() {}
 
