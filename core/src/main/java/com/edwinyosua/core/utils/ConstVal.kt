853d212ba66b8d62a3304515555d3ccd4eebf674
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-11-22 07:31:09 +0800

    Create ConstVal.kt

---
+++ core/src/main/java/com/edwinyosua/core/utils/ConstVal.kt
@@ -0,0 +1,10 @@
+package com.edwinyosua.core.utils
+
+import com.edwinyosua.core.BuildConfig
+
+
+object ConstVal {
+
+    const val BASE_URL = BuildConfig.BASE_URL
+    const val API_KEY = BuildConfig.API_KEY
+}
