a1729db42fc6daf943b48078a6b7630f6b9ebcf5
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-11-23 02:35:35 +0800

    Delete ResourceResponse.kt
    no use since we're not gonna use NetworkBoundResources and RemoteDataSources

--- core/src/main/java/com/edwinyosua/core/data/remote/network/ResourceResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.edwinyosua.core.data.remote.network
-
-sealed class ResourceResponse<T>(val data: T? = null, val message: String? = null) {
-    class Success<T>(data: T) : ResourceResponse<T>(data)
-    class Error<T>(message: String, data: T? = null) : ResourceResponse<T>(data, message)
-    class Loading<T>(data: T? = null) : ResourceResponse<T>(data)
-}
