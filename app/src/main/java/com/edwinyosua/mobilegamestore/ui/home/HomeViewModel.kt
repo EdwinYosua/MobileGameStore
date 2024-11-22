1c67c80d48902211d6eb8c28413723f0ab5a370e
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-11-23 03:04:11 +0800

    Update HomeViewModel.kt

--- app/src/main/java/com/edwinyosua/mobilegamestore/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/edwinyosua/mobilegamestore/ui/home/HomeViewModel.kt
@@ -2,9 +2,9 @@ package com.edwinyosua.mobilegamestore.ui.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.edwinyosua.core.data.repository.GameListRepository
+import com.edwinyosua.core.domain.home.usecase.GameUseCase
 
-class HomeViewModel(private val gameRepo: GameListRepository) : ViewModel() {
+class HomeViewModel(gameRepo: GameUseCase) : ViewModel() {
 
     val gameList = gameRepo.getGameList().asLiveData()
 }
