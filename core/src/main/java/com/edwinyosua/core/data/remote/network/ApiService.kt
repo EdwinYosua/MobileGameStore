7556e3756aced68a8fcfe3d704ae1b5a7e4b87cd
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-12-18 10:23:29 +0800

    Update ApiService.kt

--- core/src/main/java/com/edwinyosua/core/data/remote/network/ApiService.kt
+++ core/src/main/java/com/edwinyosua/core/data/remote/network/ApiService.kt
@@ -8,13 +8,13 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("games")
+    @GET("/api/games")
     suspend fun getGameList(
         @Query("page_size") pageSize: Int = 10,
         @Query("key") key: String = ConstVal.API_KEY
     ): RawgApiResponse
 
-    @GET("games/{id}")
+    @GET("/api/games/{id}")
     suspend fun getGameDescription(
         @Path("id") id: Int,
         @Query("key") key: String = ConstVal.API_KEY
